ea7c5b75831be5ab764c88174045da9e0f9cc965
lpardey <lpardey@outlook.com>
2023-06-10 00:06:44 +0000

    Base methods and schemas

--- app/tests/test_all_ok.py
+++ app/tests/test_all_ok.py
@@ -1,6 +1,21 @@
-# An empty test, to be able to run something with pytest : )
-# Remove me when you have something in place
+from bs4 import BeautifulSoup
+
+from app.logic.schemas import Chapter, Image
 
 
-def test_all_ok():
+def test_create_directory(web_data: BeautifulSoup) -> None:
+    assert True
+
+
+def test_get_chapters(web_data: BeautifulSoup) -> list[Chapter]:
+    assert True
+
+
+def test_get_images(web_data: BeautifulSoup) -> list[Image]:
+    """Get a list of images"""
+    pass
+
+
+def test_create_zip_file(chapters: list[Chapter]) -> None:
+    """Generate a zip file inside a directory for every chapter of the manga"""
     assert True
