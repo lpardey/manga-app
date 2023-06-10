fb22c968cb8f42fa3b377841ed54dda6864af684
lpardey <lpardey@outlook.com>
2023-06-10 19:10:25 +0000

    fix downloader class def and create dir mthd

--- app/logic/app.py
+++ app/logic/app.py
@@ -13,9 +13,7 @@ class DownloaderException(Exception):
 
 
 class DownloaderExceptionMissingTitle(DownloaderException):
-    def __init__(
-        self, message: str = "Couldn't retrieve the Manga's title from the website to create the directory file"
-    ) -> None:
+    def __init__(self, message: str = "Couldn't retrieve the Manga's title from the website") -> None:
         self.message = message
         super().__init__(self.message)
 
@@ -28,7 +26,6 @@ DESKTOP_PATH = os.path.expanduser("~/Desktop")  # Supports Unix or Windows (afte
 
 
 class Downloader(ABC):
-    @abstractmethod
     def __init__(self, web_data: BeautifulSoup, dir_path: str = DESKTOP_PATH) -> None:
         super().__init__()
         self.web_data = web_data
@@ -58,30 +55,23 @@ class Downloader(ABC):
 
 
 class Manganato(Downloader):
-    def __init__(self, web_data: BeautifulSoup, dir_path: str) -> None:
-        super().__init__()
-        self.web_data = web_data
-        self.dir_path = dir_path
-
     def create_directory(self) -> None:
         """Generate a directory named after the manga"""
         scraped_data = self.web_data.find("title")
-        dir_name = scraped_data.name
-        if dir_name is None:  # TODO: Ask user for a title name to create the dir instead of raising exception
+        if scraped_data is None:  # TODO: Ask user for a title name to create the dir instead of raising exception
             raise DownloaderExceptionMissingTitle()
 
-        path = os.path.join(self.dir_path, dir_name)
         try:
+            dir_name = scraped_data.string.removesuffix("Manga Online Free - Manganato").strip()
+            path = os.path.join(self.dir_path, dir_name)
             os.mkdir(path)
         except DownloaderException as e:
             raise DownloaderExceptionUnexpected(str(e))
 
-    @abstractmethod
     def get_chapters(self) -> list[Chapter]:
         """Pull data out of website and return a list of Chapter objects."""
         pass
 
-    @abstractmethod
     def get_images(self) -> list[Image]:
         """Get a list of images"""
         pass
