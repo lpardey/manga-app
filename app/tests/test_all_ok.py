c54283e722a0b6ff550540c6e02e7a71f3fbf290
lpardey <lpardey@outlook.com>
2023-06-10 00:47:57 +0000

    add some comments

--- app/tests/test_all_ok.py
+++ app/tests/test_all_ok.py
@@ -4,18 +4,22 @@ from app.logic.schemas import Chapter, Image
 
 
 def test_create_directory(web_data: BeautifulSoup) -> None:
+    """Generate a directory named after the manga's title"""
     assert True
 
 
 def test_get_chapters(web_data: BeautifulSoup) -> list[Chapter]:
+    """Pull data out of website and return a list of Chapter objects."""
     assert True
 
 
 def test_get_images(web_data: BeautifulSoup) -> list[Image]:
     """Get a list of images"""
-    pass
+    assert True
 
 
 def test_create_zip_file(chapters: list[Chapter]) -> None:
-    """Generate a zip file inside a directory for every chapter of the manga"""
+    """Generate a zip file inside the directory for every chapter of the manga"""
+    # Create zip file
+    # Add chapter to zip file
     assert True
