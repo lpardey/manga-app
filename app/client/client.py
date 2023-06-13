9d8def19dc773a3323d09b0235cde447f982b4cf
lpardey <lpardey@outlook.com>
2023-06-13 01:24:38 +0000

    update get html doc func

--- app/client/client.py
+++ app/client/client.py
@@ -3,10 +3,9 @@ import requests
 
 
 class DownloaderClient:
-    def get_html_doc(url: str) -> str:
+    def get_data(url: str) -> requests.Response:
         response = requests.get(url)
-        html_doc = response.text
-        return html_doc
+        return response
 
     def get_web_data(html_doc: str) -> BeautifulSoup:
         return BeautifulSoup(html_doc, "lxml")
