c9aef52dbb364d340038c2965932019e19c60cd7
lpardey <lpardey@outlook.com>
2023-06-19 16:54:22 +0000

    Fix main and added chapter and range features

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-from logic.app import DownloaderExceptionUrlWithoutCoverage, Manganato
-from parser.parser import get_parser, parse_url
+from logic.app import Downloader, Manganato
+from parser.parser import get_parser, parse_url, parse_chapter_range
 
 
 def main() -> None:
     parser = get_parser()
-    url = parse_url(parser)
-    if not url:
-        raise DownloaderExceptionUrlWithoutCoverage()
+    args = parser.parse_args()
+    url = parse_url(args.url)
+    chapters = args.chapter
+    chapter_range = parse_chapter_range(args.chapter_range)
     html_doc = requests.get(url).text
     web_data = BeautifulSoup(html_doc, "html.parser")
-    downloader = Manganato(web_data)
+    downloader = Manganato(web_data, chapters, chapter_range)
     downloader.mangadanga()
 
 
