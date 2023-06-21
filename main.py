cad379fe9b8fb706bce0482d09f6f60d22b08274
lpardey <lpardey@outlook.com>
2023-06-21 22:49:24 +0000

    add class method get downloader

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from bs4 import BeautifulSoup
+# Standard Library
+from parser.parser import get_parser, parse_chapter_range, parse_url
+
+# Dependencies
 import requests
+from bs4 import BeautifulSoup
+
+# From apps
 from logic.app import Downloader, Manganato
-from parser.parser import get_parser, parse_url, parse_chapter_range
 
 
 def main() -> None:
     parser = get_parser()
     args = parser.parse_args()
-    url = parse_url(args.url)
+    url, domain = parse_url(args.url)
+    directory_path = args.path
     chapters = args.chapter
     chapter_range = parse_chapter_range(args.chapter_range)
     html_doc = requests.get(url).text
     web_data = BeautifulSoup(html_doc, "html.parser")
-    downloader = Manganato(web_data, chapters, chapter_range)
+    downloader = Downloader.get_downloader(domain, web_data, directory_path, chapters, chapter_range)
     downloader.mangadanga()
 
 
