4f70d40ba937d6775949e4aa0dfb2d840eeb38d8
lpardey <lpardey@outlook.com>
2023-06-21 00:44:15 +0000

    refactor

--- logic/utils.py
+++ logic/utils.py
@@ -2,6 +2,29 @@
 import platform
 
 
+class DownloaderException(Exception):
+    pass
+
+
+class DownloaderExceptionUrlWithoutCoverage(DownloaderException):
+    def __init__(self, message: str = "URL not registered in MangaDanga!") -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+class DownloaderExceptionInvalidPattern(DownloaderException):
+    def __init__(
+        self,
+        message: str = "Invalid argument pattern. A valid pattern consists of two numbers where first one is smaller than the second ,e.g., '1-8'",
+    ) -> None:
+        self.message = message
+        super().__init__(self.message)
+
+
+class DownloaderExceptionUnexpected(DownloaderException):
+    pass
+
+
 def get_forbidden_chars() -> set[str] | None:
     operating_system = platform.system()
     os_to_forbidden_chars = {
