7d4dfc41b661ce030567ec5dd7f38f3df746ae5f
lpardey <lpardey@outlook.com>
2023-06-21 00:53:20 +0000

    update parser

--- parser/parser.py
+++ parser/parser.py
@@ -1,12 +1,12 @@
-import re
+# Standard Library
+import urllib.parse
 from argparse import ArgumentParser
-from pydantic import BaseModel
-from urllib.parse import urlparse
-from logic.app import (
-    DownloaderExceptionInvalidPattern,
-    DownloaderExceptionUrlWithoutCoverage,
-)
 
+# Dependencies
+from pydantic import BaseModel
+
+# From apps
+from logic.utils import DownloaderExceptionInvalidPattern, DownloaderExceptionUrlWithoutCoverage
 
 VALID_URLS = {"chapmanganato.com"}
 
@@ -15,8 +15,11 @@ class MangaDanga(BaseModel):
     name: str = "MangaDanga"
     description: str = "Download your favorite graphic novels by entering its url. A directory named after the novel will be created along with zip files for its chapter/s with its corresponding images. By default, the downloaded files will be stored where you execute the program. Enjoy!"
     epilog: str = "Chachi"
-    chapter_help: str = "Display the chapter/s required to download. It takes one or more numbers"
-    range_chapters_help: str = (
+    url_help: str = "Display a url of the graphic novel to download"
+    path_help: str = "Display the system path where the files will be stored"
+    threads_help: str = "Display a number of threads to use"
+    c_help: str = "Display the chapter/s required to download. It takes one or more numbers"
+    r_help: str = (
         "Display the range of chapters required to download. It takes two numbers, the first smaller than the second"
     )
 
@@ -28,16 +31,17 @@ def get_parser() -> ArgumentParser:
         description=program.description,
         epilog=program.epilog,
     )
-    group = parser.add_mutually_exclusive_group()
-    parser.add_argument("url", help="Display a url of the graphic novel to download")
-    parser.add_argument("-t", "--threads", default=0, type=int, help="Display a number of threads to use")
-    group.add_argument("-c", "--chapter", nargs="+", type=int, help=program.chapter_help)
-    group.add_argument("-r", "--chapter_range", nargs=2, type=int, help=program.range_chapters_help)
+    parser.add_argument("url", help=program.url_help)
+    parser.add_argument("-p", "--path", nargs="?", default=".", help=program.path_help)
+    parser.add_argument("-t", "--threads", default=0, type=int, help=program.threads_help)
+    exclusive_group = parser.add_mutually_exclusive_group()
+    exclusive_group.add_argument("-c", "--chapter", nargs="+", type=float, help=program.c_help)
+    exclusive_group.add_argument("-r", "--chapter_range", nargs=2, type=float, help=program.r_help)
     return parser
 
 
-def parse_url(url: str) -> str | None:
-    domain = urlparse(url).netloc
+def parse_url(url: str) -> str:
+    domain = urllib.parse.urlparse(url).netloc
     if domain in VALID_URLS:
         return url
     else:
