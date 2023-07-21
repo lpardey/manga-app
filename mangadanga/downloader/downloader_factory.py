9ae182d979f57e48743df787aa80b97b2f547737
lpardey <lpardey@outlook.com>
2023-07-21 22:40:02 +0000

    update downloader factory

--- mangadanga/downloader/downloader_factory.py
+++ mangadanga/downloader/downloader_factory.py
@@ -1,6 +1,6 @@
 from typing import Type
 import urllib.parse
-from .exceptions import DownloaderExceptionUrlWithoutCoverage
+from .exceptions import DownloaderException
 from .base import Downloader, DownloaderConfig
 from .drivers import Mangadoom, Manganato, Mangatown, Asurascans
 
@@ -20,7 +20,24 @@ def get_domain(url: str) -> str:
 
 def downloader_factory(config: DownloaderConfig, downloaders: set[Type[Downloader]] = DOWNLOADERS) -> Downloader:
     domain = get_domain(config.url)
-    for downloader_cls in downloaders:
-        if domain in downloader_cls.DOMAINS:
-            return downloader_cls(config)
-    raise DownloaderExceptionUrlWithoutCoverage()
+
+    # for downloader_cls in downloaders:
+    #     if domain in downloader_cls.DOMAINS:
+    #         return downloader_cls(config)
+
+    try:
+        downloader_cls = next(downloader for downloader in downloaders if domain in downloader.DOMAINS)
+        return downloader_cls(config)
+    except StopIteration:
+        valid_urls = sum(((list(downloader.DOMAINS)) for downloader in downloaders), start=[])
+        message_lines = (
+            [
+                f"'{config.url}' is not covered (yet) by Mangadanga.",
+                "",
+                "Valid domain names:",
+            ]
+            + [" - " + valid_url for valid_url in valid_urls]
+            + ["", "Our door is always open for a good cup of coffee.", "buymeacoffee@needmoney.co"]
+        )
+        message = "\n".join(message_lines)
+        raise DownloaderException(message)
