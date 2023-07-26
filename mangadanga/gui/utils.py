8d90780129c50b21c17a4ad873700257ca84d846
lpardey <lpardey@outlook.com>
2023-07-26 19:55:47 +0000

    update exception message

--- mangadanga/gui/utils.py
+++ mangadanga/gui/utils.py
@@ -3,13 +3,11 @@ from ..downloader.exceptions import DownloaderException
 
 def validate_non_empty(field: str, name: str) -> None:
     if not field:
-        raise DownloaderException(f"{name} field is missing.")
+        raise DownloaderException(f"'{name}' is missing.")
 
 
 def validate_numeric(field: str, name: str) -> None:
     try:
         float(field)
     except ValueError:
-        raise DownloaderException(f"{name} field must be numeric.")
-    
-
+        raise DownloaderException(f"'{name}' must be numeric.")
