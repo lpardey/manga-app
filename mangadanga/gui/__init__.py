101c69b8a577def34c25b680e4370925df3a060f
lpardey <lpardey@outlook.com>
2023-08-01 20:33:01 +0000

    add event classes

--- mangadanga/gui/__init__.py
+++ mangadanga/gui/__init__.py
@@ -1,9 +1,29 @@
 from .style import GUIStyle
 from .widgets import MainWindow
 from .mangadanga_gui import MangadangaGUI
+from .events import (
+    EventManager,
+    EVENT_MANAGER,
+    Event,
+    OnCloseProgress,
+    OnMangaInfoUpdate,
+    OnChapterDownloadFinished,
+    OnDownloadFinished,
+    OnDownloadFinishedGUI,
+    OnStartDownload,
+)
 
 __all__ = [
     "GUIStyle",
     "MainWindow",
     "MangadangaGUI",
+    "EventManager",
+    "EVENT_MANAGER",
+    "Event",
+    "OnCloseProgress",
+    "OnMangaInfoUpdate",
+    "OnChapterDownloadFinished",
+    "OnDownloadFinished",
+    "OnDownloadFinishedGUI",
+    "OnStartDownload",
 ]
