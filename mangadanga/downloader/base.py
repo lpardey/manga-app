2f4b740adbb876775ce6dd0000a2fb340a861191
lpardey <lpardey@outlook.com>
2023-08-02 21:34:26 +0000

    update get chapter index func and major refactor

--- mangadanga/downloader/base.py
+++ mangadanga/downloader/base.py
@@ -40,6 +40,25 @@ class Downloader(ABC):
         self.chapter_selection_strategy = chapters_selection_factory(config.chapter_strategy)
         self.event_manager = event_manager
 
+    async def download(self) -> None:
+        """Creates a directory and downloads chapters, in other words: MangaDanga!"""
+        try:
+            logger.info(f"Scrapping information for: {self.config.url}")
+            web_data = await self.scrape_url(self.config.url)
+            sanitized_title = self.get_sanitized_title(web_data)
+            self.create_directory(sanitized_title)
+            all_chapters = self.scrape_all_chapters_data(web_data)
+            await self.start_download(sanitized_title, all_chapters)
+        except Exception as e:
+            status = "error"
+            message = str(e)
+            logger.exception(e)
+        else:
+            status = "success"
+            message = ""
+        finally:
+            OnDownloadFinished(self.event_manager).emit(status, message)
+
     async def scrape_url(self, url: str) -> BeautifulSoup:
         # Creates a context manager to execute the request
         async with aiohttp.ClientSession(headers=self.get_headers()) as session:
@@ -49,30 +68,45 @@ class Downloader(ABC):
                 web_data = BeautifulSoup(html_doc, "html.parser")
                 return web_data
 
-    def create_directory(self, dir_name: str) -> None:
-        """Creates a directory on the path specified (default path: '.'). Returns directory path"""
-        path = os.path.join(self.config.path, dir_name)
-        if not os.path.isdir(path):
-            os.makedirs(path)
-
     @classmethod
     def get_headers(cls) -> dict[str, str]:
         basic_headers = {"User-Agent": "Mozilla/5.0", "Accept-Encoding": "gzip, deflate"}
         basic_headers.update(cls.EXTRA_HEADERS)
         return basic_headers
 
-    @staticmethod
-    def get_chapter_index(data: ResultSet, separators: list[str]) -> ChapterIndex:
-        final_path = PurePath(data["href"]).name
-        if not separators:
-            return ChapterIndex(final_path)
-        final_path_substrings = final_path.split(separators[0])
-        if len(separators) == 2:
-            chapter_index_substrings = final_path_substrings[1].split(separators[1])
-            chapter_index = ChapterIndex(chapter_index_substrings[1])
-        else:
-            chapter_index = ChapterIndex(final_path_substrings[1])
-        return chapter_index
+    def get_sanitized_title(self, web_data: BeautifulSoup) -> str:
+        title = self.get_title(web_data)
+        logger.info(f"Title: {title}")
+        sanitized_title = utils.format_name(title)
+        return sanitized_title
+
+    def create_directory(self, dir_name: str) -> None:
+        """Creates a directory on the path specified (default path: '.'). Returns directory path"""
+        path = os.path.join(self.config.path, dir_name)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+    def scrape_all_chapters_data(self, web_data: BeautifulSoup) -> dict[ChapterIndex, str]:
+        scrape_chapters_info_start = time.time()
+        all_chapters = self.get_all_chapters_to_url(web_data)
+        scrape_chapters_info_end = time.time()
+        ellapsed_time = scrape_chapters_info_end - scrape_chapters_info_start
+        logger.info(f"Scrapped chapters info in: {ellapsed_time:.2f} seconds")
+        return all_chapters
+
+    async def start_download(self, sanitized_title: str, all_chapters: dict[ChapterIndex, str]) -> None:
+        title = sanitized_title.replace("_", " ")
+        logger.info(f"Starting download for: {title}")
+        download_chapters_start = time.time()
+        chapter_number_to_url = self.get_chapter_number_to_url(all_chapters)
+        chapters_tasks = [
+            self.process_chapter(index, url, sanitized_title) for index, url in chapter_number_to_url.items()
+        ]
+        OnMangaInfoUpdate(self.event_manager).emit(len(chapters_tasks))
+        await gather_with_concurrency(self.config.threads, *chapters_tasks)
+        download_chapters_end = time.time()
+        ellapsed_time = download_chapters_end - download_chapters_start
+        logger.info(f"Finished downloading: {title} in {ellapsed_time:.2f} seconds")
 
     def get_chapter_number_to_url(self, all_chapters: dict[ChapterIndex, str]) -> dict[ChapterIndex, str]:
         filtered_chapters = {
@@ -81,6 +115,29 @@ class Downloader(ABC):
             if self.chapter_selection_strategy.chapter_in_selection(chapter)
         }
         return filtered_chapters
+
+    async def process_chapter(self, index: int, chapter_url: str, manga_title: str) -> None:
+        chapter_data = await self.scrape_url(chapter_url)
+        chapter_filename = self.get_chapter_filename(index, chapter_data)
+        chapter_full_path = os.path.join(self.config.path, manga_title, chapter_filename)
+        logger.info(f"Downloading chapter: {chapter_filename}")
+        with ZipFile(chapter_full_path, "w") as zipf:
+            await self.download_chapter(chapter_data, zipf)
+        OnChapterDownloadFinished(self.event_manager).emit()
+
+    async def download_chapter(self, data: BeautifulSoup, zipf: ZipFile) -> None:
+        """Gets chapter data, creates a zip file by its name, and downloads and stores its images to the zip file"""
+        images_urls = await self.get_images_src(data)
+        images = await self.get_all_chapter_images(images_urls)
+        for image_filename, image_data in images:
+            zipf.writestr(image_filename, image_data)
+
+    async def get_all_chapter_images(self, urls: list[str]) -> list[tuple[str, bytes]]:
+        get_images_tasks = [self.download_image(url) for url in urls]
+        images = await asyncio.gather(*get_images_tasks)
+        image_names = [self.get_image_name(index, url) for index, url in enumerate(urls)]
+        named_images = list(zip(image_names, images))
+        return named_images
 
     async def download_image(self, image_url: str) -> bytes:
         """Downloads an image from a url and returns it as bytes"""
@@ -96,74 +153,26 @@ class Downloader(ABC):
         image_name = f"{index:04}{image_extension}"
         return image_name
 
-    async def get_all_chapter_images(self, urls: list[str]) -> list[tuple[str, bytes]]:
-        get_images_tasks = [self.download_image(url) for url in urls]
-        images = await asyncio.gather(*get_images_tasks)
-        image_names = [self.get_image_name(index, url) for index, url in enumerate(urls)]
-        named_images = list(zip(image_names, images))
-        return named_images
+    @staticmethod
+    def get_chapter_index(data: ResultSet, sep: list[str]) -> str:
+        final_path = PurePath(data["href"]).name
+        if not sep:
+            chapter_index = final_path
+        elif len(sep) == 2:
+            chapter_index = final_path.split(sep[0])[1].split(sep[1])[1]
+        else:
+            chapter_index = final_path.split(sep[0])[1]
 
-    async def download_chapter(self, data: BeautifulSoup, zipf: ZipFile) -> None:
-        """Gets chapter data, creates a zip file by its name, and downloads and stores its images to the zip file"""
-        images_urls = await self.get_images_src(data)
-        images = await self.get_all_chapter_images(images_urls)
-        for image_filename, image_data in images:
-            zipf.writestr(image_filename, image_data)
-
-    async def process_chapter(self, index: int, chapter_url: str, manga_title: str) -> None:
-        chapter_data = await self.scrape_url(chapter_url)
-        chapter_filename = self.get_chapter_filename(index, chapter_data)
-        chapter_full_path = os.path.join(self.config.path, manga_title, chapter_filename)
-        logger.info(f"Downloading chapter: {chapter_filename}")
-        with ZipFile(chapter_full_path, "w") as zipf:
-            await self.download_chapter(chapter_data, zipf)
-        OnChapterDownloadFinished(self.event_manager).emit()
-
-    # TODO: Independent decorators for logger and for time.time()
-    async def download(self) -> None:
-        """Creates a directory and downloads chapters, in other words: MangaDanga!"""
-        status = "success"
-        message = ""
-        try:
-            logger.info(f"Scrapping information for: {self.config.url}")
-            web_data = await self.scrape_url(self.config.url)
-            title = self.get_title(web_data)
-            logger.info(f"Title: {title}")
-            sanitized_title = utils.format_name(title)
-            self.create_directory(sanitized_title)
-            # timed_get_all_chapters_to_url
-            scrap_chapters_info_start = time.time()
-            all_chapters = self.get_all_chapters_to_url(web_data)
-            scrap_chapters_info_end = time.time()
-            ellapsed_time = scrap_chapters_info_end - scrap_chapters_info_start
-            logger.info(f"Scrapped chapters info in: {ellapsed_time:.2f} seconds")
-            #
-            logger.info(f"Starting download for: {title}")
-            # Otra funcion
-            download_chapters_start = time.time()
-            chapter_number_to_url = self.get_chapter_number_to_url(all_chapters)
-            chapters_tasks = [
-                self.process_chapter(index, url, sanitized_title) for index, url in chapter_number_to_url.items()
-            ]
-            OnMangaInfoUpdate(self.event_manager).emit(len(chapters_tasks))
-            await gather_with_concurrency(self.config.threads, *chapters_tasks)
-            download_chapters_end = time.time()
-            ellapsed_time = download_chapters_end - download_chapters_start
-            logger.info(f"Finished downloading: {title} in {ellapsed_time:.2f} seconds")
-            #
-            # y nos olvidamos del tiempo medio por capítulo
-            # average_time_per_chapter = ellapsed_time / len(chapter_number_to_url)
-            # logger.info(f"Average time per chapter: {average_time_per_chapter:.2f} seconds")
-        except Exception as e:
-            status = "error"
-            message = str(e)
-            logger.exception(e)
-        finally:
-            OnDownloadFinished(self.event_manager).emit(status, message)
+        return chapter_index
 
     @abstractmethod
     def get_title(self, data: BeautifulSoup) -> str:
         """Scrapes a title"""
+        pass
+
+    @abstractmethod
+    def get_all_chapters_to_url(self, data: BeautifulSoup) -> dict[ChapterIndex, str]:
+        """Scrapes chapters urls. Returns a dict of chapter index to chapters urls"""
         pass
 
     @abstractmethod
@@ -174,9 +183,4 @@ class Downloader(ABC):
     @abstractmethod
     async def get_images_src(self, data: BeautifulSoup) -> list[str]:
         """Scrapes images urls"""
-        pass
-
-    @abstractmethod
-    def get_all_chapters_to_url(self, data: BeautifulSoup) -> dict[ChapterIndex, str]:
-        """Scrapes chapters urls. Returns a dict of chapter index to chapters urls"""
         pass
