ba15fb76d042c0c11a33ae91974906d2e5256f7b
lpardey <lpardey@outlook.com>
2023-08-02 00:44:44 +0000

    update get all chap to url method

--- mangadanga/downloader/drivers/mangadoom.py
+++ mangadanga/downloader/drivers/mangadoom.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from pathlib import PurePath
 
 from .. import utils
 
@@ -22,7 +21,8 @@ class Mangadoom(Downloader):
     def get_all_chapters_to_url(self, data: BeautifulSoup) -> dict[ChapterIndex, str]:
         chapter_content = data.find(class_="chapter-list").find_all("a")
         chapter_content.reverse()
-        chapter_number_to_url = {ChapterIndex(PurePath(data["href"]).name): data["href"] for data in chapter_content}
+        separators = []
+        chapter_number_to_url = {self.get_chapter_index(data, separators): data["href"] for data in chapter_content}
         return chapter_number_to_url
 
     def get_chapter_filename(self, index: int, data: BeautifulSoup) -> str:
@@ -32,15 +32,6 @@ class Mangadoom(Downloader):
         return chapter_file
 
     async def get_images_src(self, data: BeautifulSoup) -> list[str]:
-        # try:
-        #     number_of_imgs = len(data.find(class_="selectPage pull-right chapter-page1").find_all("option"))
-        #     first_img_src = data.find(class_="img-responsive")["src"]
-        #     src_numbers_suffix = self.get_src_numbers_suffix(first_img_src)
-        #     images_src = [first_img_src]
-        #     for i in range(1, number_of_imgs):
-        #         new_suffix = str(int(src_numbers_suffix) + i).zfill(len(src_numbers_suffix))
-        #         images_src.append(first_img_src.replace(src_numbers_suffix, new_suffix))
-        # except:
         images_src = await self.get_images_src_exhaustive_search(data)
         return images_src
 
