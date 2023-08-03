9f9a634c2dd3b3c87adcda7d636ec370a5defa90
lpardey <lpardey@outlook.com>
2023-08-03 20:06:56 +0000

    refactor

--- mangadanga/gui/widgets.py
+++ mangadanga/gui/widgets.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import asyncio
 import threading
 import tkinter
@@ -13,6 +14,7 @@ from .events import (
     OnDownloadFinished,
     OnDownloadFinishedGUI,
     OnMangaInfoUpdate,
+    OnQuit,
     OnStartDownload,
 )
 from ..downloader.config import ChapterStrategyConfig
@@ -24,20 +26,18 @@ logger = logging.getLogger("MangaDanga-GUI")
 
 class MainWindow:
     def __init__(
-        self, container: tkinter.Tk, init_config: DownloaderConfig, event_manager: EventManager = EVENT_MANAGER
+        self,
+        container: tkinter.Tk,
+        init_config: DownloaderConfig,
     ) -> None:
-        self.init_config = init_config
         self.container = container
-        self.top_banner = TopBanner(container)
-        self.main_tabs = NotebookComponent(container, init_config)
-        self.download_button = DownloadButton(container, self.get_config, event_manager=event_manager)
-        self.event_manager = event_manager
+        self.init_config = init_config
+        self.event_manager = EVENT_MANAGER
+        self.top_banner = TopBanner(self.container)
+        self.main_tabs = NotebookComponent(self.container, self.init_config)
+        self.download_button = DownloadButton(self.container, self.get_config, self.event_manager)
         self.task_process_event_queue()
-        self.window_general_management()
-
-    def task_process_event_queue(self) -> None:
-        self.event_manager.process_queue()
-        self.container.after(100, self.task_process_event_queue)
+        self.set_events()
 
     def get_config(self) -> DownloaderConfig:
         url = self.main_tabs.download_tab.url_component.get_url()
@@ -46,69 +46,24 @@ class MainWindow:
         threads = self.main_tabs.settings_tab.multithreading.get_threads()
         return DownloaderConfig(url=url, path=path, chapter_strategy=chapter_strategy_config, threads=threads)
 
-    def window_general_management(self) -> None:
-        self.container.bind("<Escape>", lambda _: self.quit())
-        self.container.protocol("WM_DELETE_WINDOW", lambda: self.quit())
+    def task_process_event_queue(self) -> None:
+        self.event_manager.process_queue()
+        self.container.after(100, self.task_process_event_queue)
 
-    def quit(self) -> None:
+    def set_events(self) -> None:
+        OnQuit(self.event_manager).subscribe(self.on_quit)
+        self.container.bind("<Escape>", lambda _: OnQuit(self.event_manager).emit())
+        self.container.protocol("WM_DELETE_WINDOW", lambda: OnQuit(self.event_manager).emit())
+
+    def on_quit(self) -> None:
         exit = messagebox.askyesno(title="Exit", message="Do you want to exit MangaDanga?")
         if exit:
             self.container.destroy()
 
 
-class ProgressWindow:
-    def __init__(self, container: tkinter.Toplevel | None = None, event_manager: EventManager = EVENT_MANAGER) -> None:
-        self.container = container or tkinter.Toplevel()
-        self.container.title("Progress")
-        self.container.resizable(False, False)
-
-        progress_bar, progress_label = self.setup_widgets()
-        self.progress_bar = progress_bar
-        self.progress_label = progress_label
-
-        self.event_manager = event_manager
-        OnChapterDownloadFinished(self.event_manager).subscribe(self.on_chapter_download_finished)
-        OnMangaInfoUpdate(self.event_manager).subscribe(self.on_manga_info_updated)
-        OnCloseProgress(self.event_manager).subscribe(self.on_close_progress)
-
-    def setup_widgets(self) -> tuple[ttk.Progressbar, ttk.Label]:
-        progress_bar = ttk.Progressbar(self.container, orient="horizontal", length=300, mode="determinate")
-        progress_bar.pack()
-
-        progress_label = ttk.Label(self.container, text="Receiving manga info...")
-        progress_label.pack()
-        return progress_bar, progress_label
-
-    def update_label(self) -> None:
-        self.progress_label[
-            "text"
-        ] = f"Chapters downloaded {self.progress_bar['value']}/{self.progress_bar['maximum']}"
-
-    def on_manga_info_updated(self, chapter_count: int) -> None:
-        logger.info(f"Received manga info. Chapter count: {chapter_count}")
-        self.progress_bar["maximum"] = chapter_count
-        self.progress_bar["value"] = 0
-        self.update_label()
-
-    def on_chapter_download_finished(self) -> None:
-        self.progress_bar["value"] += 1
-        self.update_label()
-
-    def on_close_progress(self) -> None:
-        OnChapterDownloadFinished(self.event_manager).unsubscribe(self.on_chapter_download_finished)
-        OnMangaInfoUpdate(self.event_manager).unsubscribe(self.on_manga_info_updated)
-        OnCloseProgress(self.event_manager).unsubscribe(self.on_close_progress)
-        self.container.destroy()
-
-
 class TopBanner:
     def __init__(self, container: Misc) -> None:
-        self.label = ttk.Label(
-            container,
-            text="MangaDanga",
-            anchor=tkinter.CENTER,
-            style="TopBanner.TLabel",
-        )
+        self.label = ttk.Label(container, text="MangaDanga", anchor=tkinter.CENTER, style="TopBanner.TLabel")
         self.label.grid(row=0, column=0, padx=10, pady=10, sticky=tkinter.NSEW)
 
 
@@ -125,23 +80,26 @@ class DownloadButton:
         self,
         container: Misc,
         get_config: Callable[[], DownloaderConfig],
-        event_manager: EventManager = EVENT_MANAGER,
+        event_manager: EventManager,
     ) -> None:
         self.container = container
+        self.get_config = get_config
         self.event_manager = event_manager
         self.button = ttk.Button(
-            container,
+            self.container,
             text="Download",
             command=OnStartDownload(self.event_manager).emit,
             width=15,
             style="TButton",
         )
         self.button.grid(row=2, column=0, ipady=1, padx=10, pady=10)
-        self.get_config = get_config
-        self.button.bind("<Return>", lambda _: OnStartDownload(self.event_manager).emit())
+        self.set_events()
+
+    def set_events(self) -> None:
         OnStartDownload(self.event_manager).subscribe(self.on_start_download)
         OnDownloadFinished(self.event_manager).subscribe(self.on_download_finished)
         OnDownloadFinishedGUI(self.event_manager).subscribe(self.on_download_finished_gui)
+        self.button.bind("<Return>", lambda _: OnStartDownload(self.event_manager).emit())
 
     def on_start_download(self) -> None:
         try:
@@ -164,7 +122,7 @@ class DownloadButton:
         config = self.get_config()
         downloader = downloader_factory(config)
         self.button["state"] = "disabled"
-        ProgressWindow()
+        ProgressWindow(self.container, self.event_manager)
         self.threaded_coro(downloader.download())
 
     def on_download_finished(self, status: Literal["success", "warning", "error"], message: str) -> None:
@@ -187,6 +145,47 @@ class DownloadButton:
         thread.start()
 
 
+class ProgressWindow:
+    def __init__(self, container: Misc, event_manager: EventManager) -> None:
+        self.container = container
+        self.event_manager = event_manager
+        self.top_level_win = tkinter.Toplevel(self.container)
+        self.top_level_win.title("Progress")
+        self.top_level_win.resizable(False, False)
+        self.top_level_win.wm_transient(self.container)
+        self.progress_bar = ttk.Progressbar(self.top_level_win, orient="horizontal", length=300, mode="determinate")
+        self.progress_bar.pack()
+        self.progress_label = ttk.Label(self.top_level_win, text="Receiving manga info...")
+        self.progress_label.pack()
+        self.set_events()
+
+    def set_events(self) -> None:
+        OnChapterDownloadFinished(self.event_manager).subscribe(self.on_chapter_download_finished)
+        OnMangaInfoUpdate(self.event_manager).subscribe(self.on_manga_info_updated)
+        OnCloseProgress(self.event_manager).subscribe(self.on_close_progress)
+
+    def update_label(self) -> None:
+        self.progress_label[
+            "text"
+        ] = f"Chapters downloaded {self.progress_bar['value']}/{self.progress_bar['maximum']}"
+
+    def on_manga_info_updated(self, chapter_count: int) -> None:
+        logger.info(f"Received manga info. Chapter count: {chapter_count}")
+        self.progress_bar["maximum"] = chapter_count
+        self.progress_bar["value"] = 0
+        self.update_label()
+
+    def on_chapter_download_finished(self) -> None:
+        self.progress_bar["value"] += 1
+        self.update_label()
+
+    def on_close_progress(self) -> None:
+        OnChapterDownloadFinished(self.event_manager).unsubscribe(self.on_chapter_download_finished)
+        OnMangaInfoUpdate(self.event_manager).unsubscribe(self.on_manga_info_updated)
+        OnCloseProgress(self.event_manager).unsubscribe(self.on_close_progress)
+        self.top_level_win.destroy()
+
+
 class DownloadTab:
     def __init__(self, container: ttk.Notebook, init_config: DownloaderConfig) -> None:
         self.frame = ttk.Frame(container, style="TFrame")
@@ -194,9 +193,7 @@ class DownloadTab:
         self.frame.columnconfigure(1, weight=1)
         self.frame.rowconfigure(0, weight=1)
         self.frame.rowconfigure(1, weight=1)
-
         container.add(self.frame, text="Download")
-
         self.url_component = UrlComponent(self.frame)
         self.local_path_component = LocalPathComponent(self.frame, init_config)
 
@@ -205,7 +202,6 @@ class UrlComponent:
     def __init__(self, container: Misc) -> None:
         self.label_frame = ttk.Labelframe(container, text="URL", style="TLabelframe")
         self.label_frame.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky=tkinter.EW)
-
         self.url_value = tkinter.StringVar(value="https://chapmanganato.com/manga-pf958088")
         self.text = ttk.Entry(
             self.label_frame,
@@ -243,19 +239,14 @@ class LocalPathComponent:
         self.button.grid(row=1, column=1, ipady=1, padx=10, pady=10)
         self.button.bind("<Return>", lambda _: self.handle_browse_button())
 
+    def handle_browse_button(self) -> None:
+        save_to_path = filedialog.askdirectory(initialdir=self.init_config.path)
+        self.path_value.set(save_to_path)
+
     def get_path(self) -> str:
         path = self.path_value.get()
         validate_non_empty(path, "Save to")
         return path
-
-    def handle_browse_button(self) -> None:
-        try:
-            save_to_path = filedialog.askdirectory(initialdir=self.init_config.path)
-            if save_to_path != "":
-                self.path_value.set(save_to_path)
-        except Exception as e:
-            logger.exception(e)
-            messagebox.showerror(title="Error!", message=f"Something unexpected happened: {e}")
 
 
 class SettingsTab:
@@ -263,60 +254,26 @@ class SettingsTab:
         self.frame = ttk.Frame(container, style="TFrame")
         self.frame.columnconfigure(0, weight=1)
         self.frame.columnconfigure(1, weight=1)
-
         container.add(self.frame, text="Settings")
-
         self.download_management = DownloadManagementComponent(self.frame)
         self.multithreading = MultithreadingComponent(self.frame, init_config)
 
 
 class DownloadManagementComponent:
     def __init__(self, container: Misc) -> None:
-        self.chapter_list_entry: ttk.Entry
-        self.chapter_range_lower_bound_entry: ttk.Entry
-        self.chapter_range_upper_bound_entry: ttk.Entry
         self.label_frame = ttk.Labelframe(container, text="Download Management", style="TLabelframe")
         self.label_frame.grid(row=0, column=0, padx=10, pady=10, columnspan=2, sticky=tkinter.EW)
-
         self.chapter_list_input = tkinter.StringVar()
         self.chapter_range_lower_bound_input = tkinter.StringVar()
         self.chapter_range_upper_bound_input = tkinter.StringVar()
         self.radio_input = tkinter.IntVar()
         self.set_radiobuttons()
-
-        self.set_chapters_entry()
-
-        self.set_chapter_range_entry_lower_bound()
+        self.chapter_list_entry = self.set_chapters_entry()
+        self.chapter_range_lower_bound_entry = self.set_chapter_range_entry_lower_bound()
         self.set_chapter_range_separator_label()
-        self.set_chapter_range_entry_upper_bound()
+        self.chapter_range_upper_bound_entry = self.set_chapter_range_entry_upper_bound()
         self.radio_input.trace_add("write", lambda *_: self.on_radio_button_change())
         self.radio_input.set(0)
-
-    def disable_chapter_list_entry(self) -> None:
-        self.chapter_list_entry["state"] = "disabled"
-
-    def enable_chapter_list_entry(self) -> None:
-        self.chapter_list_entry["state"] = "normal"
-
-    def disable_chapter_range_entries(self) -> None:
-        self.chapter_range_lower_bound_entry["state"] = "disabled"
-        self.chapter_range_upper_bound_entry["state"] = "disabled"
-
-    def enable_chapter_range_entries(self) -> None:
-        self.chapter_range_lower_bound_entry["state"] = "normal"
-        self.chapter_range_upper_bound_entry["state"] = "normal"
-
-    def on_radio_button_change(self) -> None:
-        index = self.radio_input.get()
-        if index == 0:
-            self.disable_chapter_list_entry()
-            self.disable_chapter_range_entries()
-        elif index == 1:
-            self.enable_chapter_list_entry()
-            self.disable_chapter_range_entries()
-        elif index == 2:
-            self.disable_chapter_list_entry()
-            self.enable_chapter_range_entries()
 
     def set_radiobuttons(self) -> None:
         chapter_options = ["All chapters", "Chapter/s", "Chapter range"]
@@ -333,39 +290,42 @@ class DownloadManagementComponent:
             )
             radiobutton.grid(row=index, column=0, padx=10, pady=10, sticky=tkinter.W)
 
-    def set_chapters_entry(self) -> None:
-        self.chapter_list_entry = ttk.Entry(
+    def set_chapters_entry(self) -> ttk.Entry:
+        chapter_list_entry = ttk.Entry(
             self.label_frame,
             textvariable=self.chapter_list_input,
             width=10,
             font=("consolas", 8),
             style="TEntry",
         )
-        self.chapter_list_entry.grid(row=1, column=2, columnspan=3, ipady=2, padx=10, pady=10, sticky=tkinter.EW)
+        chapter_list_entry.grid(row=1, column=2, columnspan=3, ipady=2, padx=10, pady=10, sticky=tkinter.EW)
+        return chapter_list_entry
 
-    def set_chapter_range_entry_lower_bound(self) -> None:
-        self.chapter_range_lower_bound_entry = ttk.Entry(
+    def set_chapter_range_entry_lower_bound(self) -> ttk.Entry:
+        chapter_range_lower_bound_entry = ttk.Entry(
             self.label_frame,
             textvariable=self.chapter_range_lower_bound_input,
             width=3,
             font=("consolas", 8),
             style="TEntry",
         )
-        self.chapter_range_lower_bound_entry.grid(row=2, column=2, ipady=2, padx=10, pady=10, sticky=tkinter.EW)
+        chapter_range_lower_bound_entry.grid(row=2, column=2, ipady=2, padx=10, pady=10, sticky=tkinter.EW)
+        return chapter_range_lower_bound_entry
 
     def set_chapter_range_separator_label(self) -> None:
         self.separator_label = ttk.Label(self.label_frame, text="To", anchor=tkinter.CENTER, style="Generic.TLabel")
         self.separator_label.grid(row=2, column=3, padx=10, pady=10)
 
-    def set_chapter_range_entry_upper_bound(self) -> None:
-        self.chapter_range_upper_bound_entry = ttk.Entry(
+    def set_chapter_range_entry_upper_bound(self) -> ttk.Entry:
+        chapter_range_upper_bound_entry = ttk.Entry(
             self.label_frame,
             textvariable=self.chapter_range_upper_bound_input,
             width=3,
             font=("consolas", 8),
             style="TEntry",
         )
-        self.chapter_range_upper_bound_entry.grid(row=2, column=4, ipady=2, padx=10, pady=10, sticky=tkinter.EW)
+        chapter_range_upper_bound_entry.grid(row=2, column=4, ipady=2, padx=10, pady=10, sticky=tkinter.EW)
+        return chapter_range_upper_bound_entry
 
     def get_chapter_selection_strategy(self) -> ChapterStrategyConfig:
         strategies = {0: "all", 1: "list", 2: "range"}
@@ -386,7 +346,7 @@ class DownloadManagementComponent:
         raw_chapters = self.chapter_list_input.get().strip().replace(" ", ",")
         validate_non_empty(raw_chapters, "Chapter/s")
         chapters_list = raw_chapters.split(",")
-        clean_chapters = [chapter for chapter in chapters_list if chapter]
+        clean_chapters = [chapter for chapter in chapters_list if validate_numeric(chapter, "Chapter/s")]
         return clean_chapters
 
     def get_chapter_range_bounds(self) -> tuple[str, str]:
@@ -403,18 +363,30 @@ class DownloadManagementComponent:
         validate_numeric(raw_bound, "Chapter range bound")
         return raw_bound
 
+    def on_radio_button_change(self) -> None:
+        index = self.radio_input.get()
+        if index == 0:
+            self.chapter_list_entry["state"] = "disabled"
+            self.chapter_range_lower_bound_entry["state"] = "disabled"
+            self.chapter_range_upper_bound_entry["state"] = "disabled"
+        elif index == 1:
+            self.chapter_list_entry["state"] = "normal"
+            self.chapter_range_lower_bound_entry["state"] = "disabled"
+            self.chapter_range_upper_bound_entry["state"] = "disabled"
+        elif index == 2:
+            self.chapter_list_entry["state"] = "disabled"
+            self.chapter_range_lower_bound_entry["state"] = "normal"
+            self.chapter_range_upper_bound_entry["state"] = "normal"
+
 
 class MultithreadingComponent:
     def __init__(self, container: Misc, init_config: DownloaderConfig) -> None:
         self.label_frame = ttk.Labelframe(container, text="Multithreading", style="TLabelframe")
         self.label_frame.grid(row=1, column=0, padx=10, pady=10, columnspan=2, sticky=tkinter.EW)
-
         self.label_frame.columnconfigure(0)
         self.label_frame.columnconfigure(1)
-
         self.label = ttk.Label(self.label_frame, text="Number of threads:", style="Generic.TLabel")
         self.label.grid(row=0, column=0, padx=10, pady=10, sticky=tkinter.W)
-
         self.combo_box = ttk.Combobox(self.label_frame, values=list(range(1, 11)), width=3, font=("consolas", 8))
         self.combo_box.set(init_config.threads)
         self.combo_box.grid(row=0, column=1, ipady=2, padx=10, pady=10)
