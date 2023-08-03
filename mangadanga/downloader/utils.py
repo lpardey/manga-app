7e8513527298b5d619a6fd16d91b1873c1f04642
lpardey <lpardey@outlook.com>
2023-08-03 20:02:18 +0000

    refactor

--- mangadanga/downloader/utils.py
+++ mangadanga/downloader/utils.py
@@ -2,6 +2,16 @@
 import platform
 
 
+def format_name(name: str) -> str:
+    forbidden_chars = get_forbidden_chars()
+    if not forbidden_chars:
+        return name
+    name = name.strip()
+    for char in forbidden_chars:
+        name = name.replace(char, "_")
+    return name
+
+
 def get_forbidden_chars() -> set[str]:
     operating_system = platform.system()
     default = {"/", " "}
@@ -11,13 +21,3 @@ def get_forbidden_chars() -> set[str]:
         "Darwin": {"/", ":", " "},
     }
     return os_to_forbidden_chars[operating_system] if operating_system in os_to_forbidden_chars else default
-
-
-def format_name(name: str) -> str:
-    forbidden_chars = get_forbidden_chars()
-    if not forbidden_chars:
-        return name
-    name = name.strip()
-    for char in forbidden_chars:
-        name = name.replace(char, "_")
-    return name
