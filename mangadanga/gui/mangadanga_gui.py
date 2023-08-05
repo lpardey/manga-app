b2b18272ef9eba0f8224183f69b609fe3cbabd55
lpardey <112667493+lpardey@users.noreply.github.com>
2023-08-05 17:11:47 -0500

    Update mangadanga_gui.py
    refactor

--- mangadanga/gui/mangadanga_gui.py
+++ mangadanga/gui/mangadanga_gui.py
@@ -10,31 +10,32 @@ from .widgets import MainWindow
 
 class MangadangaGUI:
     def __init__(self) -> None:
-        self.container = self.set_container()
+        self.container = tkinter.Tk()
+        self.set_container_config()
         self.init_config = DownloaderConfig()
         self.widgets = MainWindow(self.container, self.init_config)
         self.styles = GUIStyle(self.container)
 
-    def set_container(self) -> tkinter.Tk:
-        container = tkinter.Tk()
-        container.title("明るい")
-        container.iconbitmap(r"C:\Users\lpard\repos\manga-app\mangadanga\gui\static\Guts.ico")  # This is for windows
-        self.main_window_column_row_config()
-        self.center_main_window()
-        container.resizable(False, False)
-        return container
+    def set_container_config(self) -> None:
+        self.container.title("明るい")
+        self.container.iconbitmap(
+            r"C:\Users\lpard\repos\manga-app\mangadanga\gui\static\Guts.ico"
+        )  # This is for windows
+        self.container_column_row_config()
+        self.center_container()
+        self.container.resizable(False, False)
 
-    def main_window_column_row_config(self) -> None:
+    def container_column_row_config(self) -> None:
         self.container.columnconfigure(0, weight=1)
         self.container.rowconfigure(0, weight=1)
         self.container.rowconfigure(1, weight=1)
         self.container.rowconfigure(2, weight=1)
 
-    def center_main_window(self) -> None:
-        main_window_width = 500
-        main_window_height = 410
+    def center_container(self) -> None:
+        container_width = 500
+        container_height = 410
         screen_width = self.container.winfo_screenwidth()
         screen_height = self.container.winfo_screenheight()
-        x_axis = int((screen_width / 2) - (main_window_width / 2))
-        y_axis = int((screen_height / 2) - (main_window_height / 2)) - 70
-        self.container.geometry(f"{main_window_width}x{main_window_height}+{x_axis}+{y_axis}")
+        x_axis = int((screen_width / 2) - (container_width / 2))
+        y_axis = int((screen_height / 2) - (container_height / 2)) - 70
+        self.container.geometry(f"{container_width}x{container_height}+{x_axis}+{y_axis}")
